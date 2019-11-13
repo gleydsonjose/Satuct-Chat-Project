e38a9ebe196d913e8a13a76f35dfae354780374c
gstfps <gleydsonjosewarface@hotmail.com>
2019-11-13 03:17:16 -0300

    Alterações no design e foi adicionado uma lista de usuários

--- chat.php
+++ chat.php
@@ -23,185 +23,98 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://fonts.googleapis.com/css?family=Arimo&display=swap" rel="stylesheet">
     <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.8.2/css/all.css' integrity='sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay' crossorigin='anonymous'>
-    <style>
-        *{
-            margin: 0px;
-            padding: 0px;
-            outline: 0;
-            font-family: 'Arimo', sans-serif;
-        }
-        ::-webkit-scrollbar{
-            width: 10px;
-        }
-        ::-webkit-scrollbar-track {
-            box-shadow: inset 0 0 3px #7e7e7e;
-        }
-        ::-webkit-scrollbar-thumb {
-            background: #29b37e;
-            box-shadow: 0 0 1px 0 #7e7e7e;
-        }
-        body{
-            background-color: #272727;
-            display: flex;
-            flex-flow: column nowrap;
-            align-items: center;
-        }
-        .mensagem-erro{
-            color: #da1313;
-            font-size: 11pt;
-            text-align: center;
-            margin-top: 10px;
-        }
-        #div-chat{
-            margin-top: 7%;
-            background-color: #f0f0f0;
-            width: 350px;
-            border-radius: 1%;
-            box-shadow: 0 0 2px 0px #c7c7c7;
-        }
-        #titulo-chat{
-            width: auto;
-            text-align: center;
-            padding: 7px 0;
-            background-color: #272727;
-            margin-bottom: 1px;
-            display: flex;
-            flex-flow: column nowrap;
-        }
-        #titulo-chat h5{
-            color: #29b37e;;
-            font-weight: normal;
-        }
-        #titulo-chat h5 i{
-            margin-right: 6px;
-            color: #29b37e;
-        }
-        #chat{
-            width: auto;
-            padding: 20px;
-            height: 300px;
-            overflow: auto;
-        }
-        #usuario-info{
-            display: flex;
-            flex-flow: row nowrap;
-            margin-bottom: 25px;
-        }
-        #usuario-info:last-child{
-            margin-bottom: 0px;
-        }
-        #nome-info{
-            display: flex;
-            flex-flow: row nowrap;
-        }
-        #nome-info h5{
-            font-size: 10pt;
-            font-weight: normal;
-        }
-        #nome-usuario-chat{
-            color: #29b37e;
-            padding-right: 6px;
-        }
-        #data-usuario-chat{
-            padding-top: 1px;
-            font-size: 9pt !important;
-            color: #7e7e7e;
-        }
-        #nome-comentario p{
-            word-wrap: break-word;
-            text-align: justify;
-            font-size: 9pt;
-            padding-top: 4px;
-        }
-        #publicar-comentario{
-            display: flex;
-            flex-flow: row nowrap;
-            background-color: gray;
-            margin-top: 1px;
-        }
-        #publicar-comentario #form-group{
-            display: flex;
-            flex-flow: row nowrap;
-            width: 100%;
-        }
-        #publicar-comentario input{
-            padding: 6px 5px;
-            border: 1px solid #b5b6b5;
-            background-color: #fff;
-            width: 100%;
-        }
-        #publicar-comentario input:focus{
-            border: 1px solid #29b37e;
-            box-shadow: 0px 0px 3px 0px #29b37e;
-            transition: ease-in 0.15s;
-        }
-        #publicar-comentario button{
-            border: none;
-            align-self: center;
-            padding: 8px 15px;;
-            background-color: #29b37e;
-            border-radius: 3px;
-            color: #fff;
-        }
-        #publicar-comentario button:hover{
-            opacity: 0.8;
-            transition: ease-in 0.15s;
-            cursor: pointer;
-        }
-        #publicar-comentario button i{
-            padding-top: 2px;
-        }
-        #sair{
-            text-decoration: none;
-            color: #c7c7c7;
-            font-size: 11pt;
-            margin-top: 5px;
-        }
-        #sair:hover{
-            color: #9d9d9d;
-            transition: ease-in 0.15s;
-            cursor: pointer;
-        }
-        #sair i{
-            margin-right: 5px;
-        }
-    </style>
+    <link rel="stylesheet" href="estilo.css">
     <title>Project Chat</title>
 </head>
 <body>
-    <section id="div-chat">
-        <div id="titulo-chat">
-            <h5><i class="fas fa-user"></i><?= $mostrar_nome["nome_usuario"] ?> - Sala 1</h5>
-            <a href="scripts/sair.php" id="sair"><i class="fas fa-sign-out-alt"></i>Logout</a>
-        </div>
+    <div id="chat-lista-pessoas-main">
+        <section id="div-chat">
+            <div id="titulo-chat">
+                <h5 id="nome-usuario-titulo"><i class="fas fa-user"></i><?= $mostrar_nome["nome_usuario"] ?></h5>
 
-        <div id="chat">
-            <?php foreach($mostrar_comentarios as $mc){ 
-            // Formatando a data recebida do banco de dados
-            $data = new DateTime($mc["data"]);
-            $horario = $data->format("d/m/Y")." - ".$data->format("H:i:s");
-            ?>
-            <div id="usuario-info">
-                <div id="nome-comentario">
-                    <div id="nome-info">
-                        <h5 id="nome-usuario-chat"><?= $mc["nome_usuario"] ?></h5>
-                        <h5 id="data-usuario-chat"><?= $horario ?></h5>
+                <div id="sala-btn-sair">
+                    <h5 id="sala-numeracao"><i class="fas fa-door-closed"></i>Você está na sala 1</h5>
+                    <a href="scripts/sair.php" id="sair"><i class="fas fa-door-open"></i>Sair</a>
+                </div>            
+            </div>
+
+            <div id="chat">
+                <?php for($c = 0; $c < 5; $c++){ ?>
+                    <div id="usuario-info">
+                        <div id="nome-mensagem">
+                            <div id="nome-info">
+                                <h5 id="nome-usuario-chat">Nome usuário</h5>
+                                <h5 id="data-usuario-chat">12/11/2019 - 00:11</h5>
+                            </div>
+
+                            <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos</p>
+                        </div>
                     </div>
+                <?php } ?>
 
-                    <p><?= $mc["comentario"] ?></p>
+                <?php foreach($mostrar_comentarios as $mc){ 
+                // Formatando a data recebida do banco de dados
+                $data = new DateTime($mc["data"]);
+                $horario = $data->format("d/m/Y")." - ".$data->format("H:i:s");
+                ?>
+                <div id="usuario-info">
+                    <div id="nome-mensagem">
+                        <div id="nome-info">
+                            <h5 id="nome-usuario-chat"><?= $mc["nome_usuario"] ?></h5>
+                            <h5 id="data-usuario-chat"><?= $horario ?></h5>
+                        </div>
+
+                        <p><?= $mc["comentario"] ?></p>
+                    </div>
                 </div>
-            </div>
-            <?php } ?>
-        </div>
-
-        <form method="POST" autocomplete="off" id="publicar-comentario">
-            <div id="form-group">
-                <input type="text" id="comentario-input">
+                <?php } ?>
             </div>
 
-            <button type="button" id="publicar"><i class="fas fa-reply"></i></button>
-        </form>
-    </section>
+            <form method="POST" autocomplete="off" id="div-publicar-mensagem">
+                <div id="form-group-publicar-mensagem">
+                    <input type="text" id="input-publicar" placeholder="Digite sua mensagem">
+                </div>
+
+                <button type="button" id="publicar"><i class="fas fa-reply"></i></button>
+            </form>
+
+            <form method="POST" autocomplete="off" id="div-escolher-sala">
+                <button type="button" id="entrar-sala">Entrar</button>
+
+                <div id="form-group-escolher-sala">
+                    <select id="select-escolher-sala">
+                        <option value="Sala 1" selected>Sala 1</option>
+                        <option value="Sala 2">Sala 2</option>
+                        <option value="Sala 3">Sala 3</option>
+                    </select>
+                </div>
+            </form>
+        </section>
+
+        <section id="div-lista-pessoas">
+            <div id="titulo-lista-pessoas">
+                <h5>Lista de pessoas nesta sala</h5>
+            </div>
+
+            <div id="lista-pessoas">
+                <?php for($c = 0; $c < 11; $c++){ ?>
+                    <div id="usuario-status">
+                        <h5 id="nome-usuario-lista-pessoas">Nome usuário</h5>
+
+                        <h5 id="status"><i class="fas fa-circle"></i> Online</h5>
+                    </div>
+                <?php } ?>
+            </div>
+        </section>
+    </div>
+
+    <div class="msg-erro-publicar-mensagem">
+        <span class='mensagem-erro'>Não é possível enviar uma mensagem em branco</span>
+    </div>
+
+    <div class="msg-erro-publicar-mensagem">
+        <span class='mensagem-erro'>Você já está nesta sala</span>
+    </div>
 
     <div id="retorno-ajax-js"></div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
@@ -209,7 +122,7 @@
         $(function(){
             // Quando o botão publicar for pressionado, será feita uma verificação se o campo está vázio, se não estiver, o comentário será enviado.
             $("#publicar").click(function(){
-                var comentario = $("#comentario-input").val();
+                var comentario = $("#input-publicar").val();
                 $.ajax({
                     url: "scripts/publicar-comentario.php",
                     type: "post",
---
+++ script_principal.js
@@ -0,0 +1,313 @@
+$(function(){
+    // Quando for clicado no botão login, será mostrado a área de login e removido a de cadastro.
+    $("#btn-area-login").click(function(){
+        $("#btn-area-cadastro").removeClass("ativo");
+        $("#cadastro-form").addClass("d-none");
+        $("#btn-area-login").addClass("ativo");
+        $("#login-form").removeClass("d-none");
+
+        // Se existir alguma mensagem de erro na tela de cadastro, ela será removida.
+        if($(".msg-erro-cadastro-1").length || $(".msg-erro-cadastro-2").length || $(".msg-erro-cadastro-3").length || $(".msg-erro-cadastro-4").length || $(".msg-erro-cadastro-5").length || $(".msg-erro-cadastro-6").length || $(".msg-erro-cadastro-7").length){
+            $("#nome-usuario-cadastro").removeClass("borda-erro");
+            $("#senha-cadastro").removeClass("borda-erro");
+            $("#repita-senha-cadastro").removeClass("borda-erro");
+
+            $(".msg-erro-cadastro-1").remove();
+            $(".msg-erro-cadastro-2").remove();
+            $(".msg-erro-cadastro-3").remove();
+            $(".msg-erro-cadastro-4").remove();
+            $(".msg-erro-cadastro-5").remove();
+            $(".msg-erro-cadastro-6").remove();
+            $(".msg-erro-cadastro-7").remove();
+        }
+
+        // Se existir uma mensagem de sucesso na tela de cadastro, ela será removida.
+        if($(".msg-sucesso-cadastro").length){
+            $(".msg-sucesso-cadastro").remove();
+        }
+    })
+
+    // Quando for clicado no botão cadastro, será mostrado a área de cadastro e removido a de login.
+    $("#btn-area-cadastro").click(function(){
+        $("#btn-area-login").removeClass("ativo");
+        $("#login-form").addClass("d-none");
+        $("#btn-area-cadastro").addClass("ativo");
+        $("#cadastro-form").removeClass("d-none");
+
+        // Se existir alguma mensagem de erro na tela de login, ela será removida.
+        if($(".msg-erro-login-1").length || $(".msg-erro-login-2").length || $(".msg-erro-login-3").length || $(".msg-erro-login-4").length){
+            $("#nome-usuario-login").removeClass("borda-erro");
+            $("#senha-login").removeClass("borda-erro");
+
+            $(".msg-erro-login-1").remove();
+            $(".msg-erro-login-2").remove();
+            $(".msg-erro-login-3").remove();
+            $(".msg-erro-login-4").remove();
+        }
+    })
+
+    // Iniciando uma instância Vue.JS
+    var login = new Vue({
+        // Ligando ao elemento login-form
+        el: "#login-form",
+
+        // Inicializando um array para dados do usuário e uma variável com URL.
+        data: {
+            log: {nome_usuario: "", senha: ""},
+            urlPost: "http://localhost/Chat-Project/scripts/login.php"
+        },
+
+        methods: {
+            EnviarLogin: function () {
+                // Quando o botão login for clicado, o gif de loading será mostrado.
+                $(".gif-loader").removeClass("d-none");
+
+                // Array com dados do usuário
+                var log_form = this.formData(this.log);
+
+                // Axios vai enviar para a URL fornecida, os dados do usuário para verificações antes de ter o login realizado.
+                axios.post(this.urlPost, log_form)
+                    .then(function(resposta){
+                        /* -------------- Nome de usuário -------------- */
+                        // Se não existir essa mensagem, ela será mostrada.
+                        // OBS: Isso é para evitar repetição da mesma mensagem.
+                        if(!$(".msg-erro-login-1").length){
+                            $(".gif-loader").after(resposta.data.nome_usuario_vazio);
+                        }
+
+                        // Se não existir no array a mensagem de campo nome de usuário vazio, ela será removida da página.
+                        if(!resposta.data.nome_usuario_vazio){
+                            $(".msg-erro-login-1").remove();
+                        }
+
+                        // Se não existir essa mensagem, ela será mostrada.
+                        // OBS: Isso é para evitar repetição da mesma mensagem.
+                        if(!$(".msg-erro-login-2").length){
+                            $(".gif-loader").after(resposta.data.nome_usuario_invalido);
+                        }
+
+                        // Se não existir no array a mensagem de campo nome de usuário inválido, ela será removida da página.
+                        if(!resposta.data.nome_usuario_invalido){
+                            $(".msg-erro-login-2").remove();
+                        }
+
+                        // Se existir alguma mensagem de erro do campo nome de usuário, o campo terá a borda vermelha, se não, a borda será removida.
+                        if($(".msg-erro-login-1").length || $(".msg-erro-login-2").length){
+                            $("#nome-usuario-login").addClass("borda-erro");
+                        }else{
+                            $("#nome-usuario-login").removeClass("borda-erro");
+                        }
+                        /* -------------- Nome de usuário -------------- */
+
+                        /* -------------- Senha -------------- */
+                        // Se não existir essa mensagem, ela será mostrada.
+                        // OBS: Isso é para evitar repetição da mesma mensagem.
+                        if(!$(".msg-erro-login-3").length){
+                            $(".gif-loader").after(resposta.data.senha_vazio);
+                        }
+
+                        // Se não existir no array a mensagem de campo senha vazio, ela será removida da página.
+                        if(!resposta.data.senha_vazio){
+                            $(".msg-erro-login-3").remove();
+                        }
+
+                        // Se não existir essa mensagem, ela será mostrada.
+                        // OBS: Isso é para evitar repetição da mesma mensagem.
+                        if(!$(".msg-erro-login-4").length){
+                            $(".gif-loader").after(resposta.data.senha_incorreta);
+                        }
+
+                        // Se não existir no array a mensagem de campo senha incorreta, ela será removida da página.
+                        if(!resposta.data.senha_incorreta){
+                            $(".msg-erro-login-4").remove();
+                        }
+
+                        // Se existir alguma mensagem de erro do campo senha, o campo terá a borda vermelha, se não, a borda será removida.
+                        if($(".msg-erro-login-3").length || $(".msg-erro-login-4").length){
+                            $("#senha-login").addClass("borda-erro");
+                        }else{
+                            $("#senha-login").removeClass("borda-erro");
+                        }
+                        /* -------------- Senha -------------- */
+
+                        // Se o retorno for igual a "sucesso", o usuário será redirecionado para a página do chat.
+                        if(resposta.data == "sucesso"){
+                            window.location.href = "chat.php";
+                        }
+                    })
+                    .finally(function(){
+                        // Logo após tudo ser realizado, o gif de loading será escondido.
+                        $(".gif-loader").addClass("d-none");
+                    })
+            },
+
+            // Pegando dados do usuário e passando para um array.
+            formData: function(dados_log) {
+                var formData = new FormData();
+                for(chave in dados_log){
+                    formData.append(chave, dados_log[chave]);
+                }
+                return formData;
+            }
+        }
+    })
+
+    // Iniciando uma instância Vue.JS
+    var cadastro = new Vue({
+        // Ligando ao elemento cadastro-form
+        el: "#cadastro-form",
+
+        // Inicializando um array para dados do usuário e uma variável com URL.
+        data: {
+            cad: {nome_usuario: "", senha: "", repita_senha: ""},
+            urlPost: "http://localhost/Chat-Project/scripts/cadastro.php"
+        },
+
+        methods: {
+            EnviarCadastro: function () {
+                // Quando o botão cadastro for clicado, o gif de loading será mostrado.
+                $('.gif-loader').removeClass('d-none');
+
+                // Array com dados do usuário
+                var cad_form = this.formData(this.cad);
+
+                // Axios vai enviar para a URL fornecida, os dados do usuário para verificações antes de ter o cadastro realizado.
+                axios.post(this.urlPost, cad_form)
+                    .then(function(resposta){
+                        /* -------------- Nome de usuário -------------- */
+                        // Se não existir essa mensagem, ela será mostrada.
+                        // OBS: Isso é para evitar repetição da mesma mensagem.
+                        if(!$(".msg-erro-cadastro-1").length){
+                            $(".gif-loader").after(resposta.data.nome_usuario_vazio);
+                        }
+
+                        // Se não existir no array a mensagem de campo nome de usuário vazio, ela será removida da página.
+                        if(!resposta.data.nome_usuario_vazio){
+                            $(".msg-erro-cadastro-1").remove();
+                        }
+
+                        // Se não existir essa mensagem, ela será mostrada.
+                        // OBS: Isso é para evitar repetição da mesma mensagem.
+                        if(!$(".msg-erro-cadastro-2").length){
+                            $(".gif-loader").after(resposta.data.nome_usuario_utilizado);
+                        }
+
+                        // Se não existir no array a mensagem de campo nome de usuário utilizado, ela será removida da página.
+                        if(!resposta.data.nome_usuario_utilizado){
+                            $(".msg-erro-cadastro-2").remove();
+                        }
+
+                        // Se não existir essa mensagem, ela será mostrada.
+                        // OBS: Isso é para evitar repetição da mesma mensagem.
+                        if(!$(".msg-erro-cadastro-3").length){
+                            $(".gif-loader").after(resposta.data.nome_usuario_caractere_erro);
+                        }
+
+                        // Se não existir no array a mensagem de campo nome de usuário caractere erro, ela será removida da página.
+                        if(!resposta.data.nome_usuario_caractere_erro){
+                            $(".msg-erro-cadastro-3").remove();
+                        }
+
+                        // Se existir alguma mensagem de erro do campo nome de usuário, o campo terá a borda vermelha, se não, a borda será removida.
+                        if($(".msg-erro-cadastro-1").length || $(".msg-erro-cadastro-2").length || $(".msg-erro-cadastro-3").length){
+                            $("#nome-usuario-cadastro").addClass("borda-erro");
+                        }else{
+                            $("#nome-usuario-cadastro").removeClass("borda-erro");
+                        }
+                        /* -------------- Nome de usuário -------------- */
+
+                        /* -------------- Senha -------------- */
+                        // Se não existir essa mensagem, ela será mostrada.
+                        // OBS: Isso é para evitar repetição da mesma mensagem.
+                        if(!$(".msg-erro-cadastro-4").length){
+                            $(".gif-loader").after(resposta.data.senha_vazio);
+                        }
+
+                        // Se não existir no array a mensagem de campo senha vazio, ela será removida da página.
+                        if(!resposta.data.senha_vazio){
+                            $(".msg-erro-cadastro-4").remove();
+                        }
+
+                        // Se não existir essa mensagem, ela será mostrada.
+                        // OBS: Isso é para evitar repetição da mesma mensagem.
+                        if(!$(".msg-erro-cadastro-5").length){
+                            $(".gif-loader").after(resposta.data.senhas_diferentes);
+                        }
+
+                        // Se não existir no array a mensagem de senhas diferentes, ela será removida da página.
+                        if(!resposta.data.senhas_diferentes){
+                            $(".msg-erro-cadastro-5").remove();
+                        }
+
+                        // Se não existir essa mensagem, ela será mostrada.
+                        // OBS: Isso é para evitar repetição da mesma mensagem.
+                        if(!$(".msg-erro-cadastro-6").length){
+                            $(".gif-loader").after(resposta.data.senha_caractere_erro);
+                        }
+
+                        // Se não existir no array a mensagem de campo senha caractere erro, ela será removida da página.
+                        if(!resposta.data.senha_caractere_erro){
+                            $(".msg-erro-cadastro-6").remove();
+                        }
+
+                        // Se existir alguma mensagem de erro do campo senha, o campo terá a borda vermelha, se não, a borda será removida.
+                        if($(".msg-erro-cadastro-4").length || $(".msg-erro-cadastro-5").length || $(".msg-erro-cadastro-6").length){
+                            $("#senha-cadastro").addClass("borda-erro");
+                        }else{
+                            $("#senha-cadastro").removeClass("borda-erro");
+                        }
+                        /* -------------- Senha -------------- */
+
+                        /* -------------- Repita a Senha -------------- */
+                        // Se não existir essa mensagem, ela será mostrada.
+                        // OBS: Isso é para evitar repetição da mesma mensagem.
+                        if(!$(".msg-erro-cadastro-7").length){
+                            $(".gif-loader").after(resposta.data.repita_senha_vazio);
+                        }
+
+                        // Se não existir no array a mensagem de campo repita a senha vazio, ela será removida da página.
+                        if(!resposta.data.repita_senha_vazio){
+                            $(".msg-erro-cadastro-7").remove();
+                        }
+
+                        // Se existir alguma mensagem de erro do campo senha(que não seja o erro de caractere ou campo vazio) ou repita a senha, o campo repita a senha terá a borda vermelha, se não, a borda será removida.
+                        if($(".msg-erro-cadastro-5").length || $(".msg-erro-cadastro-6").length || $(".msg-erro-cadastro-7").length){
+                            $("#repita-senha-cadastro").addClass("borda-erro");
+                        }else{
+                            $("#repita-senha-cadastro").removeClass("borda-erro");
+                        }
+                        /* -------------- Repita a Senha -------------- */
+
+                        // Se não existir essa mensagem, ela será mostrada.
+                        // OBS: Isso é para evitar repetição da mesma mensagem.
+                        // Logo após ela ser mostrada, os campos serão limpos.
+                        if(!$("#msg-sucesso-cadastro").length){
+                            $(".gif-loader").after(resposta.data.sucesso_cadastro);
+
+                            cadastro.cad.nome_usuario = "";
+                            cadastro.cad.senha = "";
+                            cadastro.cad.repita_senha = "";
+                        }
+
+                        // Se não existir no array a mensagem de sucesso cadastro, ela será removida da página.
+                        if(!resposta.data.sucesso_cadastro){
+                            $(".msg-sucesso-cadastro").remove();
+                        }
+                    })
+                    .finally(function(){
+                        // Logo após tudo ser realizado, o gif de loading será escondido.
+                        $('.gif-loader').addClass('d-none');
+                    })
+            },
+
+            // Pegando dados do usuário e passando para um array.
+            formData: function (dados_cad) {
+                formData = new FormData();
+                for(chave in dados_cad){
+                    formData.append(chave, dados_cad[chave]);
+                }
+                return formData;
+            }
+        }
+    })
+})
